61110ae2fcd78f4b06c91bb7d3df63a0e2b719c6
Kevin Conway <kevinjacobconway@gmail.com>
2013-03-08 22:07:15 -0600

    Remove: Application logic in favour of modules
    The actual byte manipulation logic has been removed. This module
    simply parses arguments and dispatches requests now.
    
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

--- hashrot.c
+++ hashrot.c
@@ -15,255 +15,113 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include "sha2/sha2.h"
-
-void right(unsigned char* hash, FILE* infile, unsigned long file_size, FILE* ofile);
-void left(unsigned char* hash, FILE* infile, unsigned long file_size, FILE* ofile);
-void print_help();
+#include "help.c"
+#include "key.c"
+#include "right.c"
+#include "left.c"
 
 int main(int argc, char *argv[]) {
 
-    unsigned char* password = NULL;
-    char* infile_name = NULL;
-    FILE* infile = NULL;
-    FILE* ofile = NULL;
-    unsigned long file_size;
-    char* ofile_name = NULL;
     unsigned char hash[64];
-    bool reverse = false;
 
-    int x = 0;
-    while(x < argc) {
+    FILE* input = NULL;
+    FILE* output = NULL;
 
-        if (strcmp(argv[x], "-r") == 0 || strcmp(argv[x], "--reverse") == 0) {
-            reverse = true;
+    char* input_file_name = NULL;
+    char* output_file_name = NULL;
+
+    unsigned long file_size;
+
+    short received_key = 0;
+    short arg = 0;
+
+    void (*encrypt)(unsigned char*, FILE*, unsigned long, FILE*) = &right;
+
+    while(arg < argc) {
+
+        if (strcmp(argv[arg], "-r") == 0 || strcmp(argv[arg], "--reverse") == 0) {
+
+            encrypt = &left;
+
         }
 
-        if (strcmp(argv[x], "-p") == 0 || strcmp(argv[x], "--password") == 0) {
-            x = x + 1;
-            password = argv[x];
+        if (strcmp(argv[arg], "-p") == 0 || strcmp(argv[arg], "--password") == 0) {
+
+            arg = arg + 1;
+
+            sha512((unsigned char*)argv[arg], (unsigned) strlen(argv[arg]), hash);
+
+            received_key = 1;
+
         }
 
-        if (strcmp(argv[x], "-i") == 0 || strcmp(argv[x], "--input") == 0) {
-            x = x + 1;
-            infile_name = argv[x];
+        if (strcmp(argv[arg], "-k") == 0 || strcmp(argv[arg], "--keyfile") == 0) {
+
+            arg = arg + 1;
+
+            hash_from_keyfile(argv[arg], hash);
+
+            received_key = 1;
+
         }
 
-        if (strcmp(argv[x], "-o") == 0 ||strcmp(argv[x], "--output") == 0) {
-            x = x + 1;
-            ofile_name = argv[x];
+        if (strcmp(argv[arg], "-i") == 0 || strcmp(argv[arg], "--input") == 0) {
+
+            arg = arg + 1;
+            input_file_name = argv[arg];
+
         }
 
-        if (strcmp(argv[x], "-h") == 0 || strcmp(argv[x], "--help") == 0) {
+        if (strcmp(argv[arg], "-o") == 0 ||strcmp(argv[arg], "--output") == 0) {
+
+            arg = arg + 1;
+            output_file_name = argv[arg];
+
+        }
+
+        if (strcmp(argv[arg], "-h") == 0 || strcmp(argv[arg], "--help") == 0) {
+
             print_help();
             return 0;
+
         }
 
-        x = x + 1;
+        arg = arg + 1;
     }
 
-    if (password == NULL) {
+    if (received_key == 0) {
+
         puts("Error: No password defined for encryption.");
         return -1;
+
     }
 
-    if (infile_name == NULL) {
+    if (input_file_name == NULL) {
+
         puts("Error: No input file defined.");
         return -1;
+
     }
 
-    if (ofile_name == NULL) {
+    if (output_file_name == NULL) {
+
         puts("Error: No output file defined.");
         return -1;
+
     }
 
-    infile = fopen(infile_name, "rb");
-    ofile = fopen(ofile_name, "wb");
+    input = fopen(input_file_name, "rb");
+    output = fopen(output_file_name, "wb");
 
-    fseek(infile, 0, SEEK_END);
-    file_size = ftell(infile);
-    rewind(infile);
+    fseek(input, 0, SEEK_END);
+    file_size = ftell(input);
+    rewind(input);
 
-    sha512(password, (unsigned) strlen(password), hash);
+    encrypt(hash, input, file_size, output);
 
-    if(reverse == false) {
-        right(hash, infile, file_size, ofile);
-        fclose(infile);
-        fclose(ofile);
-        return 0;
-    }
-
-    left(hash, infile, file_size, ofile);
-    fclose(infile);
-    fclose(ofile);
+    fclose(input);
+    fclose(output);
     return 0;
-}
-
-void right(unsigned char* hash, FILE* infile, unsigned long file_size, FILE* ofile) {
-
-    unsigned char buffer[64];
-    unsigned int chunks = file_size / 64;
-    unsigned int left_overs = file_size % 64;
-
-    int current_chunk = 0;
-    while (current_chunk < chunks) {
-
-        fread(buffer, 1, 64, infile);
-
-        // Iterate through bytes and apply modulus addition using the
-        // same byte in the SHA hash.
-        // Just to be clear, 255 + 1 == 0 using the modulus addition.
-        int position = 0;
-        while (position < 64) {
-            buffer[position] = (buffer[position] + hash[position]) % 256;
-            position = position + 1;
-        }
-
-        // This section performs the spatial rotation of each byte in reference
-        // to the key. The rotation is based on the value of the current
-        // position in the hash. The blocks are 64 bytes in length so the value
-        // of the current position in the hash must be mod 64 to keep the
-        // rotation within the bounds of the block. As a small scale
-        // illustration, take a block of length 5 with a key of length 5 using
-        // a base 5 number system:
-        //
-        // Hash to use for encryption:  [*3*, 4, 3, 2, 1]
-        // Block to be encrypted:       [*2*, 3, 0, *1*, 4]
-        // After first rotation:        [*1*, 3, 0, *2*, 4]
-        //
-        // The byte at offset 0 was swapped with the byte at offset 3. The idea
-        // here being that each byte in the sequence is swapped with the byte
-        // that has a matching offset to the same byte in the encryption key.
-        position = 0;
-        while (position < 64) {
-            unsigned char temp = buffer[hash[position] % 64];
-            buffer[hash[position] % 64] = buffer[position];
-            buffer[position] = temp;
-            position = position + 1;
-        }
-
-        // Write the newly encrypted block to the output file and generate a new
-        // hash to use as the key. The first hash used by the encryption is a
-        // direct SHA512 of the password given. The following hashes are SHA512s
-        // of the hash that came before. This way each block is encrypted using
-        // a different key, but all keys can be generated from the initial
-        // password input.
-        fwrite(buffer, 1, 64, ofile);
-        sha512(hash, 64, hash);
-        current_chunk = current_chunk + 1;
-    }
-
-    // Since not all files are sized to multiples of 64, this section deals with
-    // any remaining bytes that don't fill an entire block.
-    if (left_overs > 0) {
-
-        fread(buffer, 1, left_overs, infile);
-
-        // The same modulus addition is performed. No change from previous
-        // section.
-        int position = 0;
-        while (position < left_overs) {
-
-            buffer[position] = (buffer[position] + hash[position]) % 256;
-            position = position + 1;
-        }
-
-        // This section makes one minor change from the regular block. It uses
-        // the length of the final byte string in the modulus for the spatial
-        // rotation. This ensures that the bytes we want are not placed outside
-        // the contents of the file.
-        position = 0;
-        while (position < left_overs) {
-            unsigned char temp = buffer[hash[position] % left_overs];
-            buffer[hash[position] % left_overs] = buffer[position];
-            buffer[position] = temp;
-            position = position + 1;
-        }
-
-        fwrite(buffer, 1, left_overs, ofile);
-    }
-
-    return;
-}
-
-void left(unsigned char* hash, FILE* infile, unsigned long file_size, FILE* ofile) {
-
-    unsigned char buffer[64];
-    unsigned int chunks = file_size / 64;
-    unsigned int left_overs = file_size % 64;
-
-    unsigned int current_chunk = 0;
-    while (current_chunk < chunks) {
-
-        fread(buffer, 1, 64, infile);
-
-        // This section works identically to the matching section in the right
-        // section but does so in reverse order. This section starts at the end
-        // of the byte string and undoes the spatial rotation.
-        int position = 63;
-        while (position >= 0) {
-            unsigned char temp = buffer[hash[position] % 64];
-            buffer[hash[position] % 64] = buffer[position];
-            buffer[position] = temp;
-            position = position - 1;
-        }
-
-        // This section reverses the modulus addition performed in the right
-        // counterpart. Technically, the encryption could left and the
-        // decryption be right. One undoes the other. It's just a matter of
-        // preference.
-        position = 0;
-        while (position < 64) {
-            buffer[position] = (buffer[position] - hash[position]) % 256;
-            position = position + 1;
-        }
-
-        fwrite(buffer, 1, 64, ofile);
-        sha512(hash, 64, hash);
-        current_chunk = current_chunk + 1;
-    }
-
-    if (left_overs > 0) {
-
-        fread(buffer, 1, left_overs, infile);
-
-        int position = left_overs - 1;
-        while (position >= 0) {
-            unsigned char temp = buffer[hash[position] % left_overs];
-            buffer[hash[position] % left_overs] = buffer[position];
-            buffer[position] = temp;
-            position = position - 1;
-        }
-
-        position = 0;
-        while (position < left_overs) {
-
-            buffer[position] = (buffer[position] - hash[position]) % 256;
-            position = position + 1;
-        }
-
-        fwrite(buffer, 1, left_overs, ofile);
-    }
-
-    return;
-}
-
-void print_help() {
-
-    puts("HashRot Help:");
-    puts("");
-    puts("-p or --password:         The password used to encrypt the file.");
-    puts("-i or --input:            The file to encrypt.");
-    puts("-o or --output:           Name to use for the new encrypted file.");
-    puts("-r or --reverse:          Toggle to run the algorithm in reverse.");
-    puts("-h or --help:             Display this output.");
-    puts("");
-    puts("Example:");
-    puts("hashrot -p mysecretpassword -i plain.txt -o encrypted.txt");
-    puts("hashrot -p mysecretpassword -i encrypted.txt -i decrypted.txt -b");
-
-    return;
 }
