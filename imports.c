923e65f9984722fb5db16321fff599f2ffa9a7d0
Kevin Conway <kevinjacobconway@gmail.com>
2013-03-10 19:40:52 -0500

    Add: Imports file for common includes
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

---
+++ imports.c
@@ -0,0 +1,41 @@
+/*
+    Hashrot - A SHA512 based double-caesar cipher
+    Copyright (C) 2013  Kevin Conway
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HASHROT_IMPORTS
+#define HASHROT_IMPORTS
+
+
+// C Standard Libs
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+// Windows libs
+#include <Windows.h>
+
+// SHA Libs
+#include "sha2.h"
+
+// HashRot Libs
+#include "param.c"
+#include "key.c"
+#include "help.c"
+#include "right.c"
+#include "left.c"
+
+#endif
